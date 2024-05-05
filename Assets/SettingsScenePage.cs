e34af89dd8d5971aa5141253cdfa8052dd84141f
tay27890123 <144402123+tay27890123@users.noreply.github.com>
2024-05-05 22:10:25 +0800

    adjust

--- Assets/SettingsScenePage.cs
+++ Assets/SettingsScenePage.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SettingsScenePage : MonoBehaviour
+public class SceneConnectionPlay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public void PlayGame()
+  {
+    SceneManager.LoadSceneAsync(3);
+  }
 }
