614533dccc6a5058da56860e056245fa25269926
tay27890123 <144402123+tay27890123@users.noreply.github.com>
2024-05-05 22:18:02 +0800

    modify

--- Assets/SettingsScenePage.cs
+++ Assets/SettingsScenePage.cs
@@ -1,12 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneConnectionPlay : MonoBehaviour
-{
-  public void PlayGame()
-  {
-    SceneManager.LoadSceneAsync(3);
-  }
-}
