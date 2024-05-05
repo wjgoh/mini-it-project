ec507334ea43272028bfb143ba387030b1a752e6
tay27890123 <144402123+tay27890123@users.noreply.github.com>
2024-05-05 22:02:51 +0800

    progress setting scene

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -1,31 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Ui;
+using UnityEngine.UI; // Corrected from UnityEngine.Ui
 
 public class SoundManager : MonoBehaviour
-
 {
     [SerializeField] Slider volumeSlider;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("musicVolume")) // Corrected capitalization of 'HasKey'
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1); // Corrected parentheses and added semicolon
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
 
     public void ChangeVolume()
     {
-       AudioListen.volume = volumeSlider.value
+        AudioListener.volume = volumeSlider.value; // Corrected 'AudioListen' to 'AudioListener' and added semicolon
+        Save();
     }
 
     private void Load()
     {
-
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume"); // Added semicolon
     }
     
     private void Save()
     {
-        
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 }
+
