dfee36b97d4028697b266efa920d397e787ee942
wjgoh <69413841+wjgoh@users.noreply.github.com>
2024-05-09 22:47:15 +0800

    undo

---
+++ Assets/Code/SoundControlsettings.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Corrected from UnityEngine.Ui
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume")) // Corrected capitalization of 'HasKey'
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1); // Corrected parentheses and added semicolon
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value; // Corrected 'AudioListen' to 'AudioListener' and added semicolon
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume"); // Added semicolon
+    }
+    
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
+
