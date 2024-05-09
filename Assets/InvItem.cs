6480b06fcd5c532abaf42126dbe1a4ff0e9d4e2f
wjgoh <69413841+wjgoh@users.noreply.github.com>
2024-05-09 22:15:37 +0800

    added something

---
+++ Assets/InvItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InvItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform parentAfterDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("On Drag");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+        transform.SetParent(parentAfterDrag);
+
+    }
+}
