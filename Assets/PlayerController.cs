8e074eb5a9d665b775b0a345d8894bbb6690f633
GibsonKhoo <shenzhikhoo@gmail.com>
2024-05-11 22:08:59 +0800

    Player Idling + Movement (COMPLETED)
    100% Completed player idling and movement animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
@@ -15,52 +14,75 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    private void FixedUpdate() { 
-
-        if(movementInput != Vector2.zero){
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
             bool success = TryMove(movementInput);
 
-            if(!success) {
+            if (!success)
+            {
                 success = TryMove(new Vector2(movementInput.x, 0));
             }
 
-            if(!success){
+            if (!success)
+            {
                 success = TryMove(new Vector2(0, movementInput.y));
             }
 
-            animator.SetBool("IsPressingS", success);
-        }else {
-            animator.SetBool("IsPressingS", false);
+            SetAnimationParameters(movementInput, success);
+        }
+        else
+        {
+            SetAnimationParameters(Vector2.zero, false);
         }
     }
-    
-    private bool TryMove(Vector2 direction) {
-        if(direction != Vector2.zero){
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
             int count = rb.Cast(
                 direction,
                 movementFilter,
                 castCollisions,
                 moveSpeed * Time.fixedDeltaTime + collisionOffset);
 
-            if (count == 0){
+            if (count == 0)
+            {
                 rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
                 return true;
-            } else{
+            }
+            else
+            {
                 return false;
             }
-        }else {
+        }
+        else
+        {
             return false;
         }
     }
 
-    void OnMove(InputValue movementValue){
+    void OnMove(InputValue movementValue)
+    {
         movementInput = movementValue.Get<Vector2>();
+    }
+
+    void SetAnimationParameters(Vector2 movement, bool moving)
+    {
+        animator.SetBool("Moving", moving);
+
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
     }
 }
