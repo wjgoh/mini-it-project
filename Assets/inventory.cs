ad63dd6ba46e2297c38859ea5b8600eec616aa52
wjgoh <69413841+wjgoh@users.noreply.github.com>
2024-05-14 20:51:12 +0800

    added e key for inventory access

---
+++ Assets/inventory.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    public GameObject InventoryButton;
+    private bool menuActivated;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && menuActivated)
+        {
+            InventoryMenu.SetActive(false);
+            InventoryButton.SetActive(true);
+            menuActivated = false;
+        }
+        
+        else if(Input.GetKeyDown(KeyCode.E) && !menuActivated)
+        {
+            InventoryMenu.SetActive(true);
+            InventoryButton.SetActive(false);
+            menuActivated = true;
+        }
+    }
+}
