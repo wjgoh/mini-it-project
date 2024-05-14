92aa5b07f42e4befc7fa09d9942ce193999a5dee
tay27890123 <144402123+tay27890123@users.noreply.github.com>
2024-05-15 00:35:36 +0800

    Revert

---
+++ Assets/TAY/Code/PLAYToGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PLAYToGame : MonoBehaviour
+{
+  public void LaodScene()
+  {
+    SceneManager.LoadScene(1);
+  }
+}
+
