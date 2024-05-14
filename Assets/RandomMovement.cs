813267eadbe7ccdac3947e60575203747636ad0f
GibsonKhoo <shenzhikhoo@gmail.com>
2024-05-14 19:31:00 +0800

    Added RandomMovement
    Cow and Chicken Movement Added

---
+++ Assets/RandomMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;  // The speed at which the NPC moves
+    public float changeDirectionTime = 2f;  // Time between direction changes
+
+    private Vector2 movementDirection;  // The direction in which the NPC will move
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(ChangeDirectionRoutine());
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = movementDirection * moveSpeed;
+        spriteRenderer.flipX = rb.velocity.x <0f;
+    }
+
+    private IEnumerator ChangeDirectionRoutine()
+    {
+        while (true)
+        {
+            ChooseRandomDirection();
+            yield return new WaitForSeconds(changeDirectionTime);
+        }
+    }
+
+    private void ChooseRandomDirection()
+    {
+        // Randomly choose left or right direction
+        int direction = Random.Range(0, 2);  // 0 for left, 1 for right
+        if (direction == 0)
+        {
+            movementDirection = Vector2.left;
+        }
+        else
+        {
+            movementDirection = Vector2.right;
+        }
+    }
+}
