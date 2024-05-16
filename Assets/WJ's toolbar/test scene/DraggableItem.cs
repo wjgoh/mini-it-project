f8792860248eb8aa3182f8b79391d35959fa88ba
wjgoh <69413841+wjgoh@users.noreply.github.com>
2024-05-16 23:45:36 +0800

    (theres error in this changes)

---
+++ Assets/WJ's toolbar/test scene/DraggableItem.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Item item;
+    [Header("UI")]
+    public Image image;
+    [HideInInspector] public Transform parentAfterDrag; 
+
+    public void Start()
+    {
+        InitialiseItem(item);
+    }
+
+    public void InitialiseItem(DraggableItem newItem)
+    {
+        image.sprite = newItem.image;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag");
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+}
