6d7a367625b76faa63fe7ba19edaff90e37afe5d
wjgoh <69413841+wjgoh@users.noreply.github.com>
2024-06-09 20:51:08 +0800

    added tasks

--- Assets/hovertext.cs
+++ Assets/hovertext.cs
@@ -2,24 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hovertext : MonoBehaviour
+public class HoverText : MonoBehaviour
 {
     public GameObject hoverText;
-    
-    
+    public Sprite defaultSprite;
+    public Sprite hoverSprite;
+
+    private SpriteRenderer spriteRenderer;
+
     public void Start()
     {
         hoverText.SetActive(false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("No SpriteRenderer found on the GameObject.");
+        }
+        else
+        {
+            spriteRenderer.sprite = defaultSprite;
+        }
     }
 
     public void OnMouseOver()
     {
         hoverText.SetActive(true);
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = hoverSprite;
+        }
     }
-    
+
     public void OnMouseExit()
     {
         hoverText.SetActive(false);
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = defaultSprite;
+        }
     }
 
     // Update is called once per frame
