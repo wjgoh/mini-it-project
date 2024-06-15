93f18b2b0bc1785c54d60b7276cb718d5b321d49
wjgoh <69413841+wjgoh@users.noreply.github.com>
2024-06-15 12:16:44 +0800

    tutorial scene

--- Assets/Teleport spawn - room.cs
+++ Assets/Teleport spawn - room.cs
@@ -7,13 +7,10 @@ public class LevelMove_Ref : MonoBehaviour
 {
     public int sceneBuildIndex;
 
-    // Level move zoned enter, if collider is a player
-    // Move game to another scene
+
     private void OnTriggerEnter2D(Collider2D other) {
         print("Trigger Entered");
         
-        // Could use other.GetComponent<Player>() to see if the game object has a Player component
-        // Tags work too. Maybe some players have different script components?
         if(other.tag == "Player") {
             // Player entered, so move level
             print("Switching Scene to " + sceneBuildIndex);
